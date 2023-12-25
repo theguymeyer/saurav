21055804f45b92c7fdb8a24b97a6f82f3677a41c
Guy Meyer <guymeyer1995@gmail.com>
2023-12-25 18:31:41 +0200

    meet saurav

--- src/examples/cards/rotatingCards/RotatingCardFront.vue
+++ src/examples/cards/rotatingCards/RotatingCardFront.vue
@@ -4,46 +4,34 @@ defineProps({
     type: String,
     required: true,
   },
-  icon: {
-    type: String,
-    default: "",
-  },
-  label: {
-    type: String,
-    default: "",
-  },
+  // icon: {
+  //   type: String,
+  //   default: "",
+  // },
+  // label: {
+  //   type: String,
+  //   default: "",
+  // },
   title: {
     type: String,
     required: true,
   },
-  description: {
-    type: String,
-    required: true,
-  },
+  // description: {
+  //   type: String,
+  //   required: true,
+  // },
 });
 </script>
 <template>
   <div
-    class="front front-background"
+    class="front-background"
     :style="{
       backgroundImage: `url(${image})`,
       backgroundSize: 'cover',
     }"
   >
     <div class="card-body py-7 text-center">
-      <i v-if="icon" class="material-icons text-white text-4xl my-3">{{
-        icon
-      }}</i>
-      <p
-        v-if="label"
-        class="text-sm text-white text-uppercase font-weight-normal"
-      >
-        {{ label }}
-      </p>
       <h3 class="text-white" v-html="title" />
-      <p class="text-white opacity-8">
-        {{ description }}
-      </p>
     </div>
   </div>
 </template>
