790bc1e17b90a0e5118948ca8fc30bd08ae1e33d
Guy Meyer <guymeyer1995@gmail.com>
2023-12-27 21:28:59 +0200

    moving HomeView to PresentationView

--- src/views/Presentation/PresentationView.vue
+++ src/views/Presentation/PresentationView.vue
@@ -2,8 +2,9 @@
 import { onMounted, onUnmounted } from "vue";
 
 //example components
+import NavbarCustom from "../..//examples/navbars/NavbarCustom.vue";
 import NavbarDefault from "../..//examples/navbars/NavbarDefault.vue";
-import DefaultFooter from "../../examples/footers/FooterDefault.vue";
+import DefaultFooterSimple from "../../examples/footers/FooterCustomSimple.vue";
 import Header from "../../examples/Header.vue";
 import FilledInfoCard from "../../examples/cards/infoCards/FilledInfoCard.vue";
 
@@ -16,10 +17,17 @@ import PresentationPages from "./Sections/PresentationPages.vue";
 import PresentationExample from "./Sections/PresentationExample.vue";
 import data from "./Sections/Data/designBlocksData";
 import BuiltByDevelopers from "./Components/BuiltByDevelopers.vue";
+import TripOffering from "./Components/TripOffering.vue";
 import PresentationTestimonials from "./Sections/PresentationTestimonials.vue";
 import PresentationInformation from "./Sections/PresentationInformation.vue";
 
 //images
+import bannerAnnapurnaNighttime from "@/assets/img/annapurna-nighttime.jpg";
+import imgAnnapurnaCircuitTrek from "@/assets/img/annapurna-circuit-trek.jpg";
+import imgAnnapurnaFlags from "@/assets/img/annapurna-flags.jpg";
+import imgEverestBaseCamp from "@/assets/img/everest-base-camp.jpg";
+import imgKanchenjungaBaseCamp from "@/assets/img/kanchenjunga-base-camp.jpg";
+import imgMakaluBaseCamp from "@/assets/img/makalu-base-camp.jpg";
 import vueMkHeader from "@/assets/img/vue-mk-header.jpg";
 import wavesWhite from "@/assets/img/waves-white.svg";
 import logoBootstrap from "@/assets/img/logos/bootstrap5.jpg";
@@ -39,259 +47,184 @@ onUnmounted(() => {
   body.classList.remove("presentation-page");
   body.classList.remove("bg-gray-200");
 });
+
+var descriptionArray = ['Item 1', 'Item 2', 'Item 3'];
+
+var trip_offerings = [
+  {
+    title: "Annapurna Region",
+    upperTitle: "Favourite",
+    description: ["Annapurna Circuit", 
+      "Nar-Phu Trek", 
+      "Annapurna Base Camp", 
+      "Mardi Himal base camp trek", 
+      "Ghorepani poonhill trek", 
+      "North Annapurna Base Camp", 
+      "upper mustang trek"],
+    backgroundImage: imgAnnapurnaFlags
+  },
+  {
+    title: "Everest Region",
+    upperTitle: "Adventureous",
+    description: ["Everest Base Camp Trek", 
+      "Pikey peak",  
+      "Everest three passes trek", 
+      "Everest Base trek with gokyo Lake via cholapass"],
+    backgroundImage: imgEverestBaseCamp
+  },
+  {
+    title: "Kanchenjunga",
+    upperTitle: "Remote",
+    description: ["Kanchenjunga Base Camp"],
+    backgroundImage: imgKanchenjungaBaseCamp
+  },
+  {
+    title: "Makalu",
+    upperTitle: "Exciting",
+    description: ["Makalu Base Camp"],
+    backgroundImage: imgMakaluBaseCamp
+  }
+
+
+
+];
+
 </script>
+
+<style scoped>
+
+.titles {
+  text-shadow: 0 0 15px #000;
+  font-family: Verdana, Geneva, sans-serif;
+}
+
+#mainTitle {
+  display: 'inline-block';
+}
+
+#mainSubTitle {
+  font-size: x-large;
+  font-weight: 500;
+}
+</style>
 
 <template>
   <div class="container position-sticky z-index-sticky top-0">
     <div class="row">
       <div class="col-12">
-        <NavbarDefault :sticky="true" />
+        <NavbarCustom :sticky="true" />
       </div>
     </div>
   </div>
   <Header>
     <div
       class="page-header min-vh-75"
-      :style="`background-image: url(${vueMkHeader})`"
+      :style="{backgroundImage: 
+        `linear-gradient(153deg, rgba(0,0,0,0.15) 0%, rgba(0,0,0,0.25) 50%, rgba(0,0,0,0.15) 100%),
+        url(${imgAnnapurnaCircuitTrek})`}"
       loading="lazy"
     >
       <div class="container">
         <div class="row">
-          <div class="col-lg-7 text-center mx-auto position-relative">
+          <div class="col-lg-10 text-center mx-auto position-relative" >
             <h1
-              class="text-white pt-3 mt-n5 me-2"
-              :style="{ display: 'inline-block ' }"
+              id="mainTitle"
+              class="titles text-white pt-3 mt-n5 me-2"
             >
-              Material Kit 2
+              Adventure with Saurav
             </h1>
-            <p class="lead text-white px-5 mt-3" :style="{ fontWeight: '500' }">
-              Start the Development With Bootstrap 5 Design System inspired by
-              Material Design.
-            </p>
+            <h5
+              id="mainSubTitle"
+              class="titles lead text-white px-5 mt-2">
+              Your local guide
+            </h5>
           </div>
         </div>
       </div>
     </div>
   </Header>
 
+
   <div class="card card-body blur shadow-blur mx-3 mx-md-4 mt-n6">
-    <PresentationCounter />
-    <PresentationInformation />
-    <PresentationExample :data="data" />
-    <PresentationPages />
-    <BuiltByDevelopers />
 
     <div class="container">
-      <div class="row">
-        <div class="col-lg-4">
-          <FilledInfoCard
-            class="p-4"
-            :color="{ text: 'white', background: 'bg-gradient-success' }"
-            :icon="{ component: 'flag', color: 'white' }"
-            title="Getting Started"
-            description="Check the possible ways of working with our product and the necessary files for building your own project."
-            :action="{
-              route:
-                'https://www.creative-tim.com/learning-lab/vue/overview/material-kit/',
-              label: { text: 'Let\'s start', color: 'white' }
-            }"
-          />
-        </div>
-        <div class="col-lg-4">
-          <FilledInfoCard
-            class="px-lg-1 mt-lg-0 mt-4 p-4"
-            height="h-100"
-            :icon="{ component: 'precision_manufacturing', color: 'success' }"
-            title="Plugins"
-            description="Get inspiration and have an overview about the plugins that we
-                used to create the Material Kit."
-            :action="{
-              route:
-                'https://www.creative-tim.com/learning-lab/vue/input/material-kit/',
-              label: { text: 'Read more' }
-            }"
-          />
-        </div>
-        <div class="col-lg-4">
-          <FilledInfoCard
-            class="px-lg-1 mt-lg-0 mt-4 p-4"
-            :icon="{ component: 'receipt_long', color: 'success' }"
-            title="Utility Classes"
-            description="Material Kit is giving you a lot of pre-made elements. For those
-                who want flexibility, we included many utility classes."
-            :action="{
-              route:
-                'https://www.creative-tim.com/learning-lab/vue/utilities/material-kit/',
-              label: { text: 'Read more' }
-            }"
-          />
+      <div class="row mt-6">
+        <div class="col-lg-6 mx-auto text-center">
+          <h2 class="mb-0">Who are we?</h2>
+
+          <h6 class="text-secondary font-weight-normal mt-3">
+            <a 
+              href="https://maps.app.goo.gl/tujWZMNBX6Hdw5uP9" 
+              target="_blank"
+              :style="{ textDecoration: 'underline' }">
+              Adventure with Saurav</a> is a full service 
+              trekking company based in Kathmandu, Nepal. 
+              We specialize in small group treks and tours to the Himalayas and around Nepal.
+            </h6>
         </div>
       </div>
     </div>
+
+
+    <hr class="horizontal dark mt-5 my-1" />
+
+    <!-- <PresentationCounter /> -->
+    <PresentationInformation />
+    <!-- <PresentationExample :data="data" /> -->
+    <!-- <PresentationPages /> -->
+
+    <hr class="horizontal dark my-1" />
+
+    <div class="container">
+      <div class="row mt-6 my-2">
+        <div class="col-lg-6 mx-auto text-center">
+          <h2 class="mb-0">Checkout our trips!</h2>
+        </div>
+      </div>
+    </div>
+
+    <TripOffering 
+      v-for="(trip, index) in trip_offerings"
+      :key="index"
+      :title="trip.title"
+      :upperTitle="trip.upperTitle"
+      :description="trip.description"
+      :backgroundImage="trip.backgroundImage"
+    />
+    
     <PresentationTestimonials />
 
-    <div
-      class="container-fluid mt-sm-5 border-radius-xl"
-      :style="{
-        background: 'linear-gradient(195deg, rgb(66, 66, 74), rgb(25, 25, 25))'
-      }"
-    >
-      <div
-        class="page-header py-6 py-md-5 my-sm-3 mb-3 border-radius-xl"
-        :style="{
-          backgroundImage: `url(${wavesWhite})`
-        }"
-        loading="lazy"
-      >
-        <span class="mask bg-gradient-dark"></span>
-        <div class="container">
-          <div class="row">
-            <div class="d-flex justify-content-center p-5">
-              <div class="col-lg-8 ms-lg-5 text-center">
-                <h3 class="text-white">
-                  Do you love this awesome UI Kit from Vuejs & Bootstrap?
-                </h3>
-                <p class="text-white text-md">
-                  Cause if you do, it can be yours for FREE. Hit the button
-                  below to navigate to Creative Tim where you can <br />
-                  find the Design System in HTML. Start a new project or give an
-                  old Bootstrap project a new look!
-                </p>
-
-                <a
-                  href="https://www.creative-tim.com/product/vue-material-kit"
-                  class="btn btn-sm mb-0 bg-gradient-success px-5 py-3 mt-4"
-                  >Download Later (now)</a
-                >
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div class="container">
-      <div class="row">
-        <div class="d-flex flex-column w-100 text-center p-5 mb-8">
-          <h3>Available on these technologies</h3>
-          <div class="d-flex justify-content-center mt-3 flex-wrap">
-            <a
-              href="https://www.creative-tim.com/product/material-kit"
-              data-bs-toggle="tooltip"
-              data-bs-placement="bottom"
-              title="Bootstrap 5 - Most popular front-end component library"
-            >
-              <img
-                :src="logoBootstrap"
-                alt="title"
-                loading="lazy"
-                :style="{ height: '90px' }"
-              />
-            </a>
-            <a
-              class="opacity-5 ms-3"
-              href="#"
-              data-bs-toggle="tooltip"
-              data-bs-placement="bottom"
-              title="Coming Soon"
-            >
-              <img
-                :src="logoTailwind"
-                alt="title"
-                loading="lazy"
-                :style="{ height: '90px' }"
-              />
-            </a>
-            <a
-              href="https://www.creative-tim.com/product/vue-material-kit-pro"
-              class="mx-3"
-              data-bs-toggle="tooltip"
-              data-bs-placement="bottom"
-              title="Vue.js - Is a Progressive JavaScript Framework"
-            >
-              <img
-                :src="logoVue"
-                alt="title"
-                loading="lazy"
-                :style="{ height: '90px' }"
-              />
-            </a>
-            <a
-              class="opacity-5"
-              href="#"
-              data-bs-toggle="tooltip"
-              data-bs-placement="bottom"
-              title="Coming Soon"
-            >
-              <img
-                :src="logoAngular"
-                alt="title"
-                loading="lazy"
-                :style="{ height: '90px' }"
-              />
-            </a>
-            <a
-              href="https://www.creative-tim.com/product/material-kit-react-pro"
-              class="mx-3"
-              data-bs-toggle="tooltip"
-              data-bs-placement="bottom"
-              title="React – A JavaScript library for building user interfaces"
-            >
-              <img
-                :src="logoReact"
-                alt="title"
-                loading="lazy"
-                :style="{ height: '90px' }"
-              />
-            </a>
-            <a
-              class="opacity-5"
-              href="#"
-              data-bs-toggle="tooltip"
-              data-bs-placement="bottom"
-              title="Coming Soon"
-            >
-              <img
-                :src="logoSketch"
-                alt="title"
-                loading="lazy"
-                :style="{ height: '90px' }"
-              />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
     <div class="py-5">
       <div class="container">
         <div class="row">
           <div class="col-lg-5 ms-auto">
-            <h4 class="mb-1">Thank you for your support!</h4>
-            <p class="lead mb-0">We deliver the best web products</p>
+            <h4 class="mb-1">Follow on Social Media!</h4>
+            <!-- <p class="lead mb-0">We deliver the best web products</p> -->
           </div>
           <div class="col-lg-5 me-lg-auto my-lg-auto text-lg-end mt-5">
-            <MaterialSocialButton
+            <!-- <MaterialSocialButton
               route="https://twitter.com/intent/tweet?text=Check%20Material%20Design%20System%20made%20by%20%40CreativeTim%20%23webdesign%20%23designsystem%20%23bootstrap5&url=https%3A%2F%2Fwww.creative-tim.com%2Fproduct%2Fmaterial-design-system-pro"
               component="twitter"
               color="twitter"
               label="Tweet"
-            />
+            /> -->
             <MaterialSocialButton
-              route="https://www.facebook.com/sharer/sharer.php?u=https://www.creative-tim.com/product/material-design-system-pro"
+              route="https://www.facebook.com/simson.puri?mibextid=rS40aB7S9Ucbxw6v"
               component="facebook-square"
               color="facebook"
-              label="Share"
+              label="Facebook"
             />
-            <MaterialSocialButton
+            <!-- <MaterialSocialButton
               route=""
               component="pinterest"
               color="pinterest"
               label="Pin it"
-            />
+            /> -->
           </div>
         </div>
       </div>
     </div>
   </div>
-  <DefaultFooter />
+  <!-- TODO udpate footer to just store the date for update tracking -->
+  <DefaultFooterSimple />
 </template>
