2733415f59845f1c6c4b6d0eafb036ed50a3c6ff
Guy Meyer <guymeyer1995@gmail.com>
2024-06-16 15:27:38 +0300

    move saurav image left

--- src/views/Presentation/Sections/SauravPresentationInformation.vue
+++ src/views/Presentation/Sections/SauravPresentationInformation.vue
@@ -12,6 +12,12 @@ import sauravHeadshot from "@/assets/img/saurav-headshot.jpeg";
   <section class="my-5 py-5">
     <div class="container">
       <div class="row align-items-center">
+        <div class="col-lg-4 ms-auto ">
+          <figure class="figure">
+            <img :src='sauravHeadshot' class="figure-img img-fluid rounded" alt="Image of Saurav. A seasoned expert of the Himalayas.">
+            <!-- <figcaption class="figure-caption text-right">Saurav</figcaption> -->
+          </figure>
+        </div>
         <div class="col-lg-6 ms-auto mt-3">
           <div class="position-sticky">
             <h3 class="justify-content-center text-center">
@@ -21,12 +27,6 @@ import sauravHeadshot from "@/assets/img/saurav-headshot.jpeg";
 Saurav is a seasoned expert of the Himalayas and has been guiding people through them for over 10 years. He has embarked on numerous adventures, from exploring remote villages to discovering lesser-known trails.  Saurav is a certified guide in both <strong>English</strong> and <strong>German</strong> who has traveled the Himalayas many times. 
             </h6>
           </div>
-        </div>
-        <div class="col-lg-4 ms-auto ">
-          <figure class="figure">
-            <img :src='sauravHeadshot' class="figure-img img-fluid rounded" alt="Image of Saurav. A seasoned expert of the Himalayas.">
-            <!-- <figcaption class="figure-caption text-right">Saurav</figcaption> -->
-          </figure>
         </div>
       </div>
     </div>
