5e4eec9c0cff57962473eea6f89d04d51d090d0e
Guy Meyer <guymeyer1995@gmail.com>
2025-03-21 15:42:57 +0200

    udpate deply

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,37 @@
 #!/bin/bash
 
-mkdir deployable
-mv dist deployable/
+# First you MUST DO THE FOLLOWING:
+#	npm install
+#	npm run build
+#	rm -rf node_modules package-lock.json
+#
+# This will create the dist directory without any of the npm artifacts. Removing build items is SUPER IMPORTANT (node_modules and lock.json)
 
-git stash push -- ./deployable
+# move to dist out
+mv dist ..
+
+# go to deployment branch
 git checkout gh-pages
-git stash pop
 
-mv deployable/dist/* .
+# clean assets to ensure nothing is leftover
+rm -rf assets
+
+mv ../dist/assets .
+
+# overwrite index.html
+mv ../dist/index.html .	
+
+# overwrite icon
+mv ../dist/favicon.ico .
 
 git add --all
 git commit -m "new deploy"
 git push origin gh-pages
 
 git checkout master
+
+# cleanup
+rmdir ../dist/assets
+rmdir ../dist
+
+exit 0
